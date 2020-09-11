6be11550c7b3df12136a187b36344ee5c741e81d
Abdullahi <abdulazeezabdullahi57@gmail.com>
2020-09-11 11:31:52 +0100

    set up main layer

--- app/src/main/java/com/sample/gadleaderboard/ui/activity/MainActivity.kt
+++ app/src/main/java/com/sample/gadleaderboard/ui/activity/MainActivity.kt
@@ -2,11 +2,21 @@ package com.sample.gadleaderboard.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.sample.gadleaderboard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_fragment)
+        return NavigationUI.navigateUp(navController, null)
     }
 }
